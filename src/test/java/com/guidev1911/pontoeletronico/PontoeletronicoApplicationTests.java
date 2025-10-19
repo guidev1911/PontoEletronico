1daf931b4cfda721aeb1f2bc35bb8132eb1fa407
guidev1911 <guilhermereal1911@gmail.com>
2025-10-18 23:50:54 -0300

    inicio do projeto + dependências

---
+++ src/test/java/com/guidev1911/pontoeletronico/PontoeletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidev1911.pontoeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoeletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
